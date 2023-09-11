cde54295b61fdd00b9f144b9cfcacf9d0ed42109
Simran <simran.abbasov.00@mail.ru>
2023-09-11 18:25:19 +0400

    gh pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -11,7 +11,7 @@ import Contact from '../pages/Contact'
 const AppRouter = () => {
   return (
 
-        <BrowserRouter>
+        <BrowserRouter basename='/SimranWebDeveloper/GEGA'>
         <Header/>
         <Routes>
             <Route path='/' element={<Home/>} />
